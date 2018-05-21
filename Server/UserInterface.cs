c1c979db7f527cf0986efd7ca6bd1d586b3556e0
Rakesh <rbaruah@gmail.com>
2018-05-21 09:13:59 -0500

    created UI class for server class

---
+++ Server/UserInterface.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Server
+{
+    interface UserInterface
+    {
+        //interface methods
+        public void Display(string message)
+        {
+            Console.WriteLine(message);
+        }
+
+        public string GetUserInput()
+        {
+            string userInput = Console.ReadLine();
+            return userInput;
+        }
+    }
+}
